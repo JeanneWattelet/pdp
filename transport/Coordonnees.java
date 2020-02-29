7b38cb90c5abb8b95cd3a7d372e6b693161e91c1
JeanneWattelet <61421875+JeanneWattelet@users.noreply.github.com>
2020-02-29 17:00:21 +0100

    Add files via upload
    Entièreté des classes décrites dans le premier diagramme de classes + quelques fonctions nécessaires. Il reste à :
    ->faire des factory 
    ->résoudre le prblm de sécurité potentiel dans le equals de Trajet : vérification superficielle de l'égalité de la Map (idem Ligne et Carte)
    ->faire des CONSTANTES pour chaque moyen de transport (classe servant de liaison entre celles-là et le graphe)

--- transport/Coordonnees.java
+++ transport/Coordonnees.java
@@ -17,5 +17,30 @@ public class Coordonnees {
 		return y;
 	}
 	
+	@Override
+	public String toString() {
+		return "x : "+this.x + ", y : "+this.y;
+	}
+	
+	@Override
+	public int hashCode() {
+		return (int) ((x-y)*1000000);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj==this)
+			return true;
+		if(obj==null)
+			return false;
+		if(!(obj instanceof Coordonnees))
+			return false;
+		Coordonnees o = (Coordonnees) obj;
+		if(o.x!=this.x)
+			return false;
+		if(o.y!=this.y)
+			return false;
+		return true;
+	}
 	
 }
--- transport/Horaire.java
+++ transport/Horaire.java
@@ -34,4 +34,53 @@ public class Horaire {
 			return false;
 		return true;
 	}
+	
+	public String getStringJour() {
+		switch(jour) {
+			case 1 :
+				return "Lundi";
+			case 2 :
+				return "Mardi";
+			case 3 :
+				return "Mercredi";
+			case 4 :
+				return "Jeudi";
+			case 5 :
+				return "Vendredi";
+			case 6 :
+				return "Samedi";
+			case 7 :
+				return "Dimanche";
+			default :
+		}
+		return "Jour ferier";
+	}
+	
+	@Override
+	public String toString() {
+		return this.getStringJour()+" "+this.heure+":"+this.minute;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.jour*10000+this.heure*100+this.minute;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj==this)
+			return true;
+		if(obj==null)
+			return false;
+		if(!(obj instanceof Horaire))
+			return false;
+		Horaire o = (Horaire) obj;
+		if(o.jour!=this.jour)
+			return false;
+		if(o.heure!=this.heure)
+			return false;
+		if(o.minute!=this.minute)
+			return false;
+		return true;
+	}
 }
