482ada2f26ddda063aa3171aa5e2c49c315f9972
TheoTik <47031339+TheoTik@users.noreply.github.com>
2020-05-13 11:28:05 +0200

    Update Graphe.java

--- pdp/src/domain/Graphe.java
+++ pdp/src/domain/Graphe.java
@@ -1,131 +1,143 @@
 package domain;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.jgrapht.alg.interfaces.AStarAdmissibleHeuristic;
-import org.jgrapht.alg.shortestpath.ALTAdmissibleHeuristic;
-import org.jgrapht.alg.shortestpath.AStarShortestPath;
-import org.jgrapht.graph.*;
-import java.util.*;
+import transport.Ligne;
 
 
-public class Graphe {
+public class SerializeGrapheTrajet {
 	
-	SimpleDirectedWeightedGraph<String, DefaultWeightedEdge> g;
-	AStarAdmissibleHeuristic<String> h;
-	
-	//Contructors
-	
-	//default : creat 4 vertexs and 4 edges
-	/*public Graphe() {
-		g = new SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-		createStringGraph();
-	}*/
-	
-	//creat a graph with all map's key as vertexs and all pairs [key;map(key)] as edges with a weight of map(key(key))
-	public Graphe(HashMap<String, HashMap<String, Double>> map) {
-		g = new SimpleDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-		creatVertexsFromStringSet(map.keySet());
-		creatWeightedEdgesFromStringMap(map);
-		creatHeuristicForAStar(map.keySet());
-
-	}
-	
-	
-	
-	//default graph
-	/*public void createStringGraph() {
-        String v1 = "Gare St-Jean";
-        String v2 = "Peixotto";
-        String v3 = "v3";
-        String v4 = "v4";
-
-        // add the vertices
-        this.g.addVertex(v1);
-        this.g.addVertex(v2);
-        this.g.addVertex(v3);
-        this.g.addVertex(v4);    
-        
-        // add edges to create a circuit
-        addWeightedEdge(v1, v2, 5);
-        addWeightedEdge(v2, v3, 4);
-        addWeightedEdge(v1, v4, 8);
-        addWeightedEdge(v4, v3, 8);
-       
-        //configure the heuristic for AStar
-        HashSet<String> s = new HashSet<String>();
-        s.add(v1);
-        s.add(v2);
-		creatHeuristicForAStar(s);
-    }*/
-	
-	
-	//Used by constructor
-	
-	public void creatVertexsFromStringSet(Set<String> set) {
-		Iterator<String> i = set.iterator();
-		String stop;
-		while(i.hasNext()) {
-			stop=i.next();
-			this.g.addVertex(stop);
-		}
-	}
-	
-	public void creatWeightedEdgesFromStringMap(HashMap<String, HashMap<String, Double>> map) {
-		Set<String> set = map.keySet(); 
-		Iterator<String> i = set.iterator();
-		String from;
-		while(i.hasNext()) {
-			from=i.next();
-			String to;
-			Iterator<String> j = map.get(from).keySet().iterator();
-			while(j.hasNext()) {
-				to=j.next();
-				addWeightedEdge(from, to, map.get(from).get(to));
+	public static void serialiserGrapheTrajet(GrapheTrajet gt){
+		ObjectOutputStream oos = null;
+		
+		try {
+			final FileOutputStream fichier = new FileOutputStream("src\\saves\\"+gt.getJour()+".ser");
+			oos = new ObjectOutputStream(fichier);
+			oos.writeObject(gt);
+			oos.flush();
+		} catch (final java.io.IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (oos != null) {
+					oos.flush();
+					oos.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
 			}
 		}
 	}
 	
-	public void addWeightedEdge(String v1, String v2, double weight) {
-		g.addEdge(v1, v2);
-		g.setEdgeWeight(v1, v2, weight);
+	public static void serialiserArcTrajet(List<ArcTrajet> lat, int j){
+		ObjectOutputStream oos = null;
+		
+		try {
+			final FileOutputStream fichier = new FileOutputStream("src\\data1\\ListArcTrajetPied"+j+"bus.dat");
+			oos = new ObjectOutputStream(fichier);
+			oos.writeObject(lat);
+			oos.flush();
+		} catch (final java.io.IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (oos != null) {
+					oos.flush();
+					oos.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 	
-	
-	//For AStar
-	
-	public void creatHeuristicForAStar(Set<String> set) {
-		h = new ALTAdmissibleHeuristic<String, DefaultWeightedEdge>(g,set);
-	}
-	
-	public void astar(String from, String to) {
-        AStarShortestPath<String, DefaultWeightedEdge> astar = new AStarShortestPath<String, DefaultWeightedEdge>(g, h);
-        System.out.println("Shortest Path : "+astar.getPath(from, to));
-        System.out.println("Weight of this path : "+astar.getPathWeight(from, to));
-	}
-	
-	
-	//Override from Object
-	
-	@Override
-	public String toString() {
-		return g.toString();
-	}
-	
-	@Override
-	public int hashCode() {
-		return g.hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if(obj==this)
-			return true;
-		if(obj.getClass()!=this.getClass())
-			return false;
-		Graphe o = (Graphe) obj;
-		if(o.g!=this.g)
-			return false;
-		return true;
-	}
+	public static GrapheTrajet deserialiserGrapheTrajet(int jour){
+		GrapheTrajet gt = null;
+		
+		ObjectInputStream ois = null;
 
+	    try {
+	      final FileInputStream fichier = new FileInputStream("src\\saves\\"+jour+".ser");
+	      ois = new ObjectInputStream(fichier);
+	      gt = (GrapheTrajet)ois.readObject() ;
+	    } catch (final java.io.IOException e) {
+	      e.printStackTrace();
+	    } catch (final ClassNotFoundException e) {
+	      e.printStackTrace();
+	    } finally {
+	      try {
+	        if (ois != null) {
+	          ois.close();
+	        }
+	      } catch (final IOException ex) {
+	        ex.printStackTrace();
+	      }
+	    }
+		return gt;
+	}
+	
+	public static void saveTramEtBus(int jour) {
+		List<Ligne> tram = new ArrayList<Ligne>();
+		List<Ligne> bus = new ArrayList<Ligne>();
+		try {
+			tram = Donnees.ChargerDonnees("src\\keolis_tram", jour);
+			bus = Donnees.ChargerDonnees("src\\keolis_bus", jour);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			ObjectOutputStream save = new ObjectOutputStream(new FileOutputStream("src/dataSommetsAretes/tram" + jour + ".dat"));
+			ObjectOutputStream save2 = new ObjectOutputStream(new FileOutputStream("src/dataSommetsAretes/bus" + jour + ".dat"));
+			save.writeObject(tram);
+			save2.writeObject(bus);
+			save.close();
+			save2.close();
+		}
+		catch(Exception e) {
+			System.out.println("error : " + e.getMessage());
+		}
+	}
+	
+	public static List<Ligne> loadTram(int jour){
+		List<Ligne> tram = new ArrayList<Ligne>(); 
+		try {
+			ObjectInputStream save = new ObjectInputStream(new FileInputStream("src/dataSommetsAretes/tram" + jour + ".dat"));
+			tram = (List<Ligne>) save.readObject();
+			save.close();
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return tram;
+	}
+	
+	public static List<Ligne> loadBus(int jour){
+		List<Ligne> bus = new ArrayList<Ligne>();
+		try {
+			ObjectInputStream save2 = new ObjectInputStream(new FileInputStream("src/dataSommetsAretes/bus" + jour + ".dat"));
+			bus = (List<Ligne>) save2.readObject();
+			save2.close();
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return bus;
+	}
+	
+	public static List<ArcTrajet> loadAreteAttenteMemeEndroit(int jour) {
+		List<ArcTrajet> l = new ArrayList<ArcTrajet>();
+		try {
+			ObjectInputStream load = new ObjectInputStream(new FileInputStream("src/dataSommetsAretes/aretesAttenteMemeEndroitBus" + jour + ".dat"));
+			l = (List<ArcTrajet>) load.readObject();
+			load.close();
+		}
+		catch(Exception e) {
+			System.out.println("error : " + e.getMessage());
+		}
+		return l;
+	}
 }
